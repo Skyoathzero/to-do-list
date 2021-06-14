7bcceb50de7737ace26d156d1a293317e26aed82
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2021-06-14 13:07:43 +0700

    done adding the add object functionality

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import Task from './task'
 
 function App() {
+    const [tasks, setTasks] = useState(['a','b','c'])
+    const [count, setCount] = useState(0)
+
+    const addFromCounter = () => {
+        setTasks([...tasks,count])
+        setCount((c)=>c+1)
+    }
     return (
-        <div>
-            
-        </div>
+        <main>
+            <section className="input-container">
+                <button className="btn" onClick={addFromCounter}>new task</button>
+            </section>
+            <section className="list-container">
+                {tasks.map((task,index) =>{
+                    return(<Task key={index}/>)
+                })}
+            </section>
+        </main>
     )
 }
 
